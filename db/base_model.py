35c15e06b78b7a01f0d962bb7415d27ace6a756f
Christian Raue <captain_2@gmx.de>
2023-07-17 20:33:24 +0200

    added dates

--- db/base_model.py
+++ db/base_model.py
@@ -1,6 +1,7 @@
 from uuid import uuid4
+from datetime import datetime
 
-from peewee import Field, Model, SqliteDatabase, TextField
+from peewee import Field, Model, SqliteDatabase, TextField, DateTimeField
 
 DATABASE_FILENAME: str = "db/db.sqlite3"
 db: SqliteDatabase = SqliteDatabase(DATABASE_FILENAME)
@@ -11,6 +12,9 @@ class BaseModel(Model):
         database = db
 
     id_ = TextField(primary_key="True", default=uuid4)
+    created_at = DateTimeField(default=datetime.now)
+    updated_at = DateTimeField(default=datetime.now)
 
     def save(self, force_insert: bool = True, only: list[Field] = None):
+        self.updated_at = datetime.now()
         super().save(force_insert, only)
