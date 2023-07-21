62c7b31da4a52d33e95f14df8b183abc3867aaa6
Christian Raue <captain_2@gmx.de>
2023-07-21 15:26:49 +0200

    updated utility scripts

--- backend/manage.py
+++ backend/manage.py
@@ -1,34 +1,68 @@
-from product import Product, Tag, TagXProduct
-from db.base_model import db
-import sys
 import os
-from scraper import Scraper
+import sys
 
+from db.base_model import db
+from product import Product, Tag, TagXProduct
+from scraper import Scraper
 
 TAGS: list[str] = [
     'impact', 'intro', 'finale', 'howler'
 ]
 
 
+def db_create():
+    db.connect()
+    db.create_tables([Product, Tag, TagXProduct])
+    for tag in TAGS:
+        Tag.create(name=tag)
+    db.close()
+
+
+def db_del():
+    os.remove("backend/db/db.sqlite3")
+    with open("backend/db/db.sqlite3", 'w') as file:
+        file.write("")
+    del_plots()
+
+
+def db_recreate():
+    db_del()
+    db_create()
+
+
+def del_plots():
+    for filename in os.listdir("backend/static/product_plots"):
+        full_filename = os.path.join(
+            "backend/static/product_plots", filename
+        )
+        os.remove(full_filename)
+
+
+def create_plots():
+    products = Product.select()
+    for p in products:
+        p.create_plots()
+
+
+def scrape():
+    scraper = Scraper()
+    scraper.scrape()
+
+
 def main():
     arg = sys.argv[1]
     if arg == 'db_create':
-        db.connect()
-        db.create_tables([Product, Tag, TagXProduct])
-        for tag in TAGS:
-            Tag.create(name=tag)
-        db.close()
+        db_create()
     elif arg == 'del_plots':
-        for filename in os.listdir("static/product_plots"):
-            full_filename = os.path.join("static/product_plots", filename)
-            os.remove(full_filename)
+        del_plots()
     elif arg == 'scrape':
-        scraper = Scraper()
-        scraper.scrape()
-    elif arg == 'del_db':
-        os.remove("db/db.sqlite3")
-        with open("db/db.sqlite3", 'w') as file:
-            file.write("")
+        scrape()
+    elif arg == 'db_del':
+        db_del()
+    elif arg == 'create_plots':
+        create_plots()
+    elif arg == 'db_recreate':
+        db_recreate()
 
 
 if __name__ == "__main__":
